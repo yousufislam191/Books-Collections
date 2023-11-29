3632984f03ebdd0f3ef38258e63a5fa2ff44c4f7
Md. Yousuf Islam <63366048+yousufislam191@users.noreply.github.com>
2023-11-29 12:50:25 +0600

    Update secret.js

--- server/secret.js
+++ server/secret.js
@@ -2,7 +2,9 @@ require("dotenv").config();
 
 const serverPort = process.env.PORT || 5200;
 
-const corsOrigin = process.env.CORS_ORIGIN || "http://localhost:5173";
+// const corsOrigin = process.env.CORS_ORIGIN || "http://localhost:5173";
+
+const corsOrigin = process.env.CORS_ORIGIN || "https://books-collections-9cty.vercel.app";
 
 const mongodbUrl =
   process.env.DB_URL || "mongodb://localhost:27017/booksCollections";
