a2fe590c0e3b47dd72b910464f8a179d97902990
yousufislam191 <yousufislam337@gmail.com>
2023-11-29 11:09:31 +0600

    added vercel host

--- client/src/helper/apiConfig.js
+++ client/src/helper/apiConfig.js
@@ -3,7 +3,7 @@ let apiHostName;
 if (window.location.hostname === "localhost") {
   apiHostName = "http://localhost:5000/api"; // Set your local API URL here
 } else {
-  apiHostName = "https://task-management-system-eg5a.vercel.app/api"; // Set your Vercel API URL here
+  apiHostName = "https://books-collections.vercel.app/api"; // Set your Vercel API URL here
 }
 
 export default apiHostName;
