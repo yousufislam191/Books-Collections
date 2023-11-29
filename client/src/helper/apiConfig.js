ac442e9ef6a146297f4e0c9052bd285e55facc44
yousufislam191 <yousufislam337@gmail.com>
2023-11-29 11:17:41 +0600

    added host

--- client/src/helper/apiConfig.js
+++ client/src/helper/apiConfig.js
@@ -3,7 +3,8 @@ let apiHostName;
 if (window.location.hostname === "localhost") {
   apiHostName = "http://localhost:5000/api"; // Set your local API URL here
 } else {
-  apiHostName = "https://books-collections.vercel.app/api"; // Set your Vercel API URL here
+  apiHostName =
+    "https://books-collections-pi17g9rzc-yousufislam191.vercel.app//api"; // Set your Vercel API URL here
 }
 
 export default apiHostName;
